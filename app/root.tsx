0c333487b1f3bd4dbc1d6c846df6601f4fa16d77
roninfrodsham <roninfrodsham@gmail.com>
2022-12-12 15:06:28 +0000

    add in global stylesheet and google font

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import type { MetaFunction, LinksFunction } from "@remix-run/cloudflare";
 import {
   Links,
   LiveReload,
@@ -7,6 +7,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import stylesUrl from "~/styles/global.css";
+
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -18,6 +23,9 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet" />
         <Meta />
         <Links />
       </head>
